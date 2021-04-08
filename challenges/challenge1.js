3e5330f8ba392cbf3125fcf1646fb08e1fdda44e
teknofage <teknofage@gmail.com>
2021-04-07 23:59:43 -0700

    challenge1 completed

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -1,19 +1,34 @@
 /*
  *******************************************************************************
  * INSTRUCTIONS:
- * Follow the steps below and answer the discusssion questions that follow.
+ * Follow the steps below and answer the discussion questions that follow.
  * 
  * 1. Read over the code that follows. In what order will the outputs "Step 1",
  *    "Step 2", and "Step 3" be printed? How do you know?
+ * 
+ * 1, 3, 2, because the console.log for Step 2 is wrapped in a snooze function 
+ * that waits 2000 ms before resolving
  * 
  * 
  * 2. Run this code using `node challenge1.js`. In what order were the steps
  *    printed?
  * 
+ * 1, 3, 2
  * 
  * 3. Change the delay time in the `snooze` function from 2000 ms to 0. In what
- *    order will the steps be printed now? Why? Re-run the code again to verify
+ *    order will the steps be printed now? 
+ * 
+ * 1, 2, 3
+ * 
+ * Why? 
+ * 
+ * Because the console.log for Step 2 is no longer being held back from 
+ * resolving by 2000 ms
+ * 
+ * Re-run the code again to verify
  *    your expectation. Were you correct?
+ * 
+ * 
  * 
  *******************************************************************************
  */
@@ -22,7 +37,7 @@
 function snooze(action) {
     setTimeout(function() {
       action();
-    }, 2000);
+    }, 0);
 }
 console.log('Step 1');
 
